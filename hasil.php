3c3676919733d0144b01bdf3aa8c8f764e5033bf
Radian Yusuf Mahendra <radian.ym@gmail.com>
2020-11-02 18:26:09 +0700

    default value jika inputan kosong

--- hasil.php
+++ hasil.php
@@ -6,6 +6,12 @@ $operator = $_POST['operator'];
 $number1 = $_POST['number1'];
 $number2 = $_POST['number2'];
 
+//set value 0 jika inputan kosong
+if (empty($number1) && empty($number2)) {
+  $number1 = 0;
+  $number2 = 0;
+}
+
 $aritmatika = new Aritmatika($operator, $number1, $number2);
 echo $aritmatika->hitungAritmatika();
 
